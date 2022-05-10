e370242dab3a2c3af0f2559a3c50bb7fc936e1b7
Genil Montilde <genil.montilde@cloudemployee.co.uk>
2022-05-10 20:14:28 +0800

    added authentication endpoint login/register and passport package

---
+++ app/Http/Controllers/Auth/UserAuthsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Http\Requests\UserAuthsRequest;
+use App\Http\Requests\LoginRequest;
+
+class UserAuthsController extends Controller
+{
+    /**
+     * Register a new user.
+     *
+     * @param  App\Http\Requests\UserAuthsRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(UserAuthsRequest $request)
+    {
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        $user = User::create($data);
+        $token = $user->createToken('authToken')->accessToken;
+        return response(['user' => $user, 'token' => $token]);
+    }
+
+    /**
+     * Login User.
+     *
+     * @param  App\Http\Requests\LoginRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(LoginRequest $request)
+    {
+        $data = $request->all();
+
+        if(!auth()->attempt($data)) {
+            return response(['error_message' => 'Incorrect Details. Please try again']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
+}
