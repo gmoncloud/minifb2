79ab281f1ac891a7a6522f421c7d9d3845285fb1
Genil Montilde <genil.montilde@cloudemployee.co.uk>
2022-05-16 11:57:54 +0800

    hotfix-post-request

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('created_at', 'desc')->get();
         return response([ 'posts' =>
             PostResource::collection($posts),
             'message' => 'Success'], 200);
